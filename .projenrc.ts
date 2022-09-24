01c2500f1c267e583ba8205ceb1163f97400a7af
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2022-09-24 20:25:45 +0800

    configure projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,18 +1,39 @@
-import { awscdk } from "projen";
-const project = new awscdk.AwsCdkConstructLibrary({
-  author: "devops-at-home",
-  authorAddress: "76842834+devops-at-home@users.noreply.github.com",
-  authorName: "DevOps@Home",
-  cdkVersion: "2.1.0",
-  defaultReleaseBranch: "main",
-  eslint: false,
-  name: "cdk-ec2-octopus-deploy-tentacle",
-  projenrcTs: true,
-  repositoryUrl: "https://github.com/devops-at-home/cdk-ec2-octopus-deploy-tentacle.git",
+import { awscdk } from 'projen';
 
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'devops-at-home',
+  authorAddress: 'https://devops-at-ho.me',
+  authorName: 'DevOps@Home',
+  cdkVersion: '2.43.0',
+  defaultReleaseBranch: 'main',
+  eslint: false,
+  name: 'cdk-ec2-octopus-deploy-tentacle',
+  projenrcTs: true,
+  repositoryUrl:
+    'https://github.com/devops-at-home/cdk-ec2-octopus-deploy-tentacle.git',
+  license: 'MIT',
+  gitignore: ['.idea', '.DS_Store'],
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 120,
+      tabWidth: 2,
+      singleQuote: true,
+    },
+  },
+  githubOptions: {
+    pullRequestLint: false,
+  },
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ['devops-at-home'],
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.jest!.addTestMatch('**/?(*.)@(spec|test).[tj]s?(x)');
+
 project.synth();
